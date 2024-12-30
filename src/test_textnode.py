8141f5ec4f12064c4fe1879442632220f22d424d
seanchavez <realseanchavez@gmail.com>
2024-12-29 20:11:28 -0800

    implement TestTextNode

---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_type(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_link(self):
+        node = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.LINK, "https://www.go.dev")
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
