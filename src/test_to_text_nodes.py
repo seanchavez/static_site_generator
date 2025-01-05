a9b1150e8f92085d64adc786fcfeaec68d3afb82
seanchavez <realseanchavez@gmail.com>
2025-01-05 02:15:38 -0800

    implement text_to_textnodes

--- src/test_to_text_nodes.py
+++ src/test_to_text_nodes.py
@@ -5,8 +5,7 @@ from text_to_textnodes import text_to_textnodes
 
 class TestTextToTextNodes(unittest.TestCase):
     def test_mixed_text(self):
-        text = "This is **text** with an *italic* word and a `code block` and an\
-               ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
         result = text_to_textnodes(text)
         expected = [
             TextNode("This is ", TextType.NORMAL),
--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -1,7 +1,15 @@
 from split_nodes_delimiter import split_nodes_delimiter
 from split_nodes_image import split_nodes_image
 from split_nodes_link import split_nodes_link
+from textnode import TextNode, TextType
 
 
 def text_to_textnodes(text):
-    pass
+    split_nodes = [TextNode(text, TextType.NORMAL)]
+    for delimiter, text_type in [
+        ("**", TextType.BOLD),
+        ("*", TextType.ITALIC),
+        ("`", TextType.CODE),
+    ]:
+        split_nodes = split_nodes_delimiter(split_nodes, delimiter, text_type)
+    return split_nodes_image(split_nodes_link(split_nodes))
