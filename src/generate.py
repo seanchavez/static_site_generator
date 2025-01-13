a51da0056e548775c4f42a8a804565af8ebdb102
seanchavez <realseanchavez@gmail.com>
2025-01-13 13:17:36 -0800

    implement generate_page

--- src/generate.py
+++ src/generate.py
@@ -1,6 +1,7 @@
-from os.path import exists, isfile, join
-from os import listdir, mkdir
+from os.path import exists, isfile, join, split
+from os import listdir, mkdir, makedirs
 from shutil import rmtree, copy
+from block_helpers import markdown_to_html_node
 
 
 def copy_static(source, destination):
@@ -12,7 +13,36 @@ def copy_static(source, destination):
     else:
         contents = listdir(source)
         for item in contents:
-            if isfile(item):
-                copy(join(item, source), destination)
+            source_item, dest_item = join(source, item), join(destination, item)
+            if isfile(source_item):
+                copy(source_item, dest_item)
             else:
-                copy_static(join(source, item), join(destination, item))
+                copy_static(source_item, dest_item)
+
+
+def extract_title(markdown):
+    for line in markdown.splitlines():
+        if line.startswith("# "):
+            return line.lstrip("#").strip()
+    raise ValueError("Document must have a top level heading")
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as md_file:
+        markdown = md_file.read()
+        print(f"MARKDOWN: {markdown}")
+    with open(template_path) as tmpl_file:
+        template = tmpl_file.read()
+        print(f"TEMPLATE: {template}")
+    html = markdown_to_html_node(markdown).to_html()
+    print(f"HTML: {html}")
+    title = extract_title(markdown)
+    print(f"TITLE: {title}")
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    print(f"FINAL: {template}")
+    # if not exists(dest_path):
+    #     makedirs(dest_path)
+    with open(dest_path, "w") as dest_file:
+        dest_file.write(template)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from generate import copy_static
+from generate import copy_static, generate_page
 
 
 def main():
     copy_static("static/", "public/")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
